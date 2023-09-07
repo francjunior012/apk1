9293edeff391f1d14c863ffb3c2670c799149ad0
francjunior012 <francjunior012@gmail.com>
2023-09-07 19:26:10 +0100

    essaie1

--- index.js
+++ index.js
@@ -1,65 +1,67 @@
-let inputName = document.getElementById(`name`);
-let inputPassword = document.getElementById(`password`);
-let message = document.getElementById(`message`);
-
-// traitement au click bouton connextion
-let btnLogin = document.getElementById(`btnLogin`);
-fetch(`fichier json/fichier.json`).then(value => value.json()).then(etudiants => {
-	var tableau = JSON.stringify(etudiants);
-	localStorage.setItem(`etudiants`, tableau);
-
+// declaration des variables
+let inputName = document.getElementById("name");
+let inputPassword = document.getElementById("password");
+let message = document.getElementById("message");
+let btnLogin = document.getElementById("btnLogin");
+let btnPassword = document.getElementById("btnPassword");
+fetch("fichier json/fichier.json").then(value => value.json()).then(etudiants => {
+  var tableau = JSON.stringify(etudiants);
+  localStorage.setItem("etudiants", tableau);
 });
-btnLogin.addEventListener(`click`, function () {
-	console.log(JSON.stringify(localStorage.getItem(`etudiants`)));
-	// alert(`hello word`)
-	let name= inputName.value;
-	let password = inputPassword.value;
-	 alert(`le nom d'utilisateur est: ` + name + `le mon de passe est:  ${password}`)
-	// l'ajout du messsage d'erreur
-	// message.style.display = 'block';
-	
-	// autentification
-	var nom = document.getElementById(`name`).value;
-	var MDP =document.getElementById(`password`).value;
-	if (nom == ""|| MDP == "") {
-		alert(`veuillez remplir le champ vide svp!`);
-	
-	} //else {
-	var donner = localStorage.getItem(`etudiants`);
-	console.log(donner);
+// uliser le click au bouton
+fermer.addEventListener("click", function () {
+	form.style.display = "none";
+})
+btnPassword.addEventListener("click", function () {
+	form.style.display = "block";
+});
+btnLogin.addEventListener("click", function() {
+  var nom = inputName.value;
+	var MDP = inputPassword.value;
+	// verifier si un champs est vide
+	if (nom === "" || MDP === "") {
+		alert(`veillez remplir les champs vides `);
+	} else {
+		// declaration de la variable locale 
+    var donner = JSON.parse(localStorage.getItem("etudiants"));
+// verifier si le nom ou le prenom se retouve dans le json
+    var etudiantTrouve = false;
+    for (var i = 0; i < donner.length; i++) {
+      if (donner[i].nom === nom && donner[i].password === MDP) {
+        etudiantTrouve = true;
+        break;
+      }
+    }
+// l'etudiants trouver oui ou nom
+    if (etudiantTrouve) {
+		window.location.href = 'pages.html ';
 		
-		// }
-	var etudiantsTrouve = false;
-	// verifier les information
-
-		// for (var i = 0; i< donner.non.length; i++){
-			// if (donner[ i ].nom === Donner && donner[ i ].password === password) {
-				//  etudiantsTrouve = true;
-				//  break;
-				// console.log(donner[ i ]);
-			//  } else {
-				//  console.log(`vide`);
-			//  }
-		//  }
-		// if (etudiantsTrouve) {
-			//  alert(`autentification reussi!`);
-		// } else {
-		//   alert(`nom d'utilisatur ou mot de passe incorrect`);
-			// };
-	
-
-	// window.location.href = 'pages.html';
+    } else {
+	//   message.style.display = "block";
+    }
+  }
+});
+//  animation sur les deux boutons
+btnLogin.addEventListener("mouseover", () => {
+  btnLogin.style.color = "red";
+  btnLogin.style.backgroundColor = "#eee";
 });
 
-
-
-
-// annimation sur mon bouton conection
-btnLogin.addEventListener(`mouseover`, () => {
-	btnLogin.style.color = "red";
-	btnLogin.style.backgroundColor = `#eee`;
+btnLogin.addEventListener("mouseout", () => {
+  btnLogin.style.backgroundColor = "yellow";
+  btnLogin.style.color = "black";
 });
-btnLogin.addEventListener(`mouseout`, () => {
-	btnLogin.style.backgroundColor = `yellow`;
-	btnLogin.style.color = "black";
+// deuxiem
+btnPassword.addEventListener("mouseover", () => {
+	btnPassword.style.backgroundColor = "red";
+	btnPassword.style.color = "pink";
 });
+btnPassword.addEventListener("mouseout", () => {
+	btnPassword.style.backgroundColor = "aqua";
+	btnPassword.style.color = "black";
+	btnPassword.style.cursor="alias";
+})
+// btnPassword.addEventListener("click", (){
+// form.style.display = "block";
+// })
+// message.style.display = "block";
